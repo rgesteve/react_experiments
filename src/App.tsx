d869be6c76793046841c2898e76c1722a688581b
R. G. Esteves <rodolfo.g.esteves@intel.com>
2019-08-02 00:03:53 -0700

    Projecting react component to TS

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,45 @@ import HelloWorld from './components/HelloWorld';
 import Smiley from './components/Smiley';
 
 import classnames from 'classnames'
+import { BarChart } from 'react-d3-components' ;
 
 const App: React.FC = () => {
   let classes = classnames("btn", { "isActive" : true});
 
+  let data = [
+    { label: 'somethingA',
+      values : [
+            { x : 'SomethingA', y : 10 },
+            { x : 'SomethingB', y : 4 },
+            { x : 'SomethingC', y : 3 },
+      ]
+    },
+    { label: 'somethingB',
+      values : [
+            { x : 'SomethingA', y : 6 },
+            { x : 'SomethingB', y : 8 },
+            { x : 'SomethingC', y : 5 },
+      ]
+    },
+    { label: 'somethingC',
+      values : [
+            { x : 'SomethingA', y : 6 },
+            { x : 'SomethingB', y : 8 },
+            { x : 'SomethingC', y : 5 },
+      ]
+    },
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
+      	<BarChart
+	  data={data}
+  	  height={400}
+  	  width={600}
+  	  margin={{ top : 10, bottom : 50, left: 50, right : 10 }}
+	/>
+
         <HelloWorld greeting="Joe" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
